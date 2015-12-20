93c384b38321ffb228069e4b831ac9412b4f3aff
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2015-12-20 16:14:51 +0100

    Solution for "Pertes en bourse" game

--- puzzles/13.js
+++ puzzles/13.js
@@ -2,72 +2,40 @@
  * Pertes en bourse
  */
 
-var c   = require('../common.js');
-var log = c.log;
-
-var Grid = function () {
-    this.list = [];
-
-    this.store = function store () {
-
-    };
-
-};
-
 var App = function () {
 
     var grid = [];
 
-    function _init () {
-        var n      = parseInt(readline());
-        var inputs = readline().split(' ');
+    function _initialize () {
+        var n       = parseInt(readline());
+        var inputs  = readline().split(' ');
 
         for (var i = 0; i < n; i++) {
             var v = parseInt(inputs[i]);
-            grid.push({
-                index: i,
-                value: v
-            });
+            grid.push(v);
         }
-
     }
 
-    function _doLoop ()  {
-        var largest = 0;
+    function _doLoop () {
+        var gridLargest = 0;
 
-        grid.forEach(function (item) {
-            !item.deltas &&  (item.deltas  = []);
-            !item.largest && (item.largest = 0);
-
-            var i = item.index + 1;
-            while (grid[i]) {
-                log('delta from: ' + item.index + ' and ' + grid[i].index);
-
-                item.largest = Math.max(item.value - grid[i].value, item.largest);
-
-                item.deltas.push({
-                    with: i,
-                    delta: item.value - grid[i].value
-                });
-
-                i++;
+        grid.forEach(function (item, index) {
+            var iteLargest = 0;
+            for (var i = index + 1 ; i < grid.length ; i++) {
+                iteLargest = Math.max(item - grid[i], iteLargest);
             }
-
-            log('largest : ' + item.largest);
-            largest = Math.max(item.largest, largest);
+            gridLargest = Math.max(iteLargest, gridLargest);
         });
 
-        return -1 * largest;
+        return -1 * gridLargest;
     }
 
-    function _main () {
-        _init();
-        var largest = _doLoop();
-
-        print(largest);
+    function _start () {
+        _initialize();
+        print(_doLoop());
     }
 
-    this.start = _main;
+    this.start = _start;
 };
 
 var app = new App;
