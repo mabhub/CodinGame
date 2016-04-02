e60cc3d2b538d2f811c5e4cb2ea5d419b743631d
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-04-02 19:02:05 +0200

    Solution for "Skynet : le virus" game (1)

---
+++ puzzles/1.js
@@ -0,0 +1,83 @@
+/**
+ * Skynet : le virus
+ */
+
+var App = function () {
+
+    var links = [];
+    var exits = [];
+
+    var _inLink = (node, link) => (node === link[0] || node === link[1]);
+
+    function _isExitLink (link) {
+        let exits_num = exits.length;
+        for (let i = 0 ; i < exits_num ; i++) {
+            let exit = exits[i];
+            if (_inLink(exit, link)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    var _isAgentLink = (agent_node, link) => (agent_node === link[0] || agent_node === link[1]);
+
+    function _init () {
+        let inputs    = readline().split(' ');
+        let nodes_num = +inputs[0];
+        let links_num = +inputs[1];
+        let exits_num = +inputs[2];
+
+        for (let i = 0; i < links_num; i++) {
+            let inputs = readline().split(' ');
+            links.push([+inputs[0], +inputs[1]]);
+        }
+
+        for (let i = 0; i < exits_num; i++) {
+            exits.push(+readline());
+        }
+    }
+
+    function _deleteLink (link_index) {
+        print(links[link_index].join(' '));
+        links.splice(link_index, 1);
+    }
+
+    function _doLoop () {
+        mainloop: while (true) {
+            let agent_node = +readline();
+            let links_num   = links.length;
+
+            /**
+             * Check if agent is immediatly near exit
+             */
+            for (let i = 0 ; i < links_num ; i++) {
+                if (_isExitLink(links[i]) && _isAgentLink(agent_node, links[i])) {
+                    _deleteLink(i);
+                    continue mainloop;
+                }
+            }
+
+            /**
+             * If agent is not immediatly near exit,
+             * delete any exit link
+             */
+            for (let i = 0 ; i < links_num ; i++) {
+                if (_isExitLink(links[i])) {
+                    _deleteLink(i);
+                    continue mainloop;
+                }
+            }
+        }
+    }
+
+    function _main () {
+        _init();
+        _doLoop();
+    }
+
+    this.start = _main;
+};
+
+var app = new App;
+app.start();
