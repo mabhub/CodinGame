11de77a60f1b1e7a759fa87b5e421e4177afa87d
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-04-08 01:11:58 +0200

    Solution for "Scrabble" game (19)

---
+++ puzzles/19.js
@@ -0,0 +1,56 @@
+/**
+ * Scrabble
+ */
+
+const DICT    = [...Array(+readline())].map(_ => readline());
+const LETTERS = readline();
+
+const pts = {
+    a: 1, b: 3, c: 3, d: 2, e: 1, f: 4, g: 2, h: 4, i: 1, j: 8, k: 5, l: 1, m: 3,
+    n: 1, o: 1, p: 3, q: 10, r: 1, s: 1, t: 1, u: 1, v: 4, w: 4, x: 8, y: 4, z: 10
+};
+
+/**
+ * Compute points of word
+ */
+var points = (word) => word.split('').reduce((score, letter) => score += pts[letter], 0);
+
+/**
+ * Check if all letters from word are present within letters
+ */
+var matchLetters = (word, letters) => word.split('').reduce((bool, letter) => {
+    if (bool !== true) return false;
+
+    if (letters.search(letter) > -1) {
+        letters = letters.replace(letter, '');
+        return true;
+    }
+
+    return false;
+}, true);
+
+
+print(
+    /**
+     * Iterate over DICT and keep only the better word
+     */
+    DICT.reduce((previous, current) => {
+
+        /**
+         * Can't keep word with to many letters
+         */
+        if (current.length > LETTERS.length) return previous;
+
+        /**
+         * If current word don't match letter, delete it.
+         */
+        current = matchLetters(current, LETTERS) ? current : '';
+
+        /**
+         * If current word has more point thant previous, return it.
+         * Otherwise return previous one
+         */
+        return points(current) > points(previous) ? current : previous;
+
+    }, '')
+);
