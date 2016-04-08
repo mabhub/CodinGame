2cdde614d48df0e516a3e73845ad8d7cd7970ee6
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-04-08 21:30:36 +0200

    Variant for "Le cadeau" game (20)

--- puzzles/20.js
+++ puzzles/20.js
@@ -2,39 +2,13 @@
  * Le cadeau
  */
 
-// Total number of Oods.
-var oods    =+readline();
+var oods    =+readline(); // Total number of Oods.
+var amount  =+readline(); // Money to pay
 
-// Money to pay
-var amount  =+readline();
+const wallets  = [...Array(oods)].map(_ => +readline()).sort((a, b) => a - b); // Content of each one wallets
+const arraySum = (arr) => arr.reduce((a, b) => a + b, 0);
 
-// Content of each one wallets
-var wallets = [...Array(oods)].map(_ => +readline()).sort((a, b) => a - b);
-
-// Polyfill
-Array.prototype.sum = function () { return this.reduce((a, b) => a + b, 0); }
-
-var getContribs = (wallet) => {
-    /**
-     * part: what the Ood should pay if he has enough money
-     */
-    let part = Math.floor(amount / oods);
-
-    /**
-     * contrib: how much the Ood will really pay.
-     */
-    let contrib = Math.min(part, wallet);
-
-    /**
-     * Remove the Ood and his participation from the remaining
-     */
-    amount -= contrib;
-    oods   -= 1; // oods--;
-
-    return contrib;
-};
-
-if (amount > wallets.sum()) {
+if (amount > arraySum(wallets)) {
     /**
      * Oods don't have money enough to pay for the gift.
      */
@@ -43,6 +17,23 @@ if (amount > wallets.sum()) {
     /**
      * Oods have money enough, compute & print the part of each.
      */
-    let contribs = wallets.map(getContribs);
-    contribs.forEach((contrib) => print(contrib));
+    wallets.forEach((wallet) => {
+        /**
+         * part: what the Ood should pay if he has enough money
+         */
+        let part = Math.floor(amount / oods);
+
+        /**
+         * contrib: how much the Ood will really pay.
+         */
+        let contrib = Math.min(part, wallet);
+
+        /**
+         * Remove the Ood and his participation from the remaining
+         */
+        amount -= contrib;
+        oods   -= 1; // oods--;
+
+        print(contrib);
+    });
 }
