ebadd4a82816b834decddbee89d57a8fcd5a3f2f
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-04-08 20:53:56 +0200

    Variant for "Le cadeau" game (20)

--- puzzles/20.js
+++ puzzles/20.js
@@ -2,36 +2,47 @@
  * Le cadeau
  */
 
+// Total number of Oods.
 var oods    =+readline();
+
+// Money to pay
 var amount  =+readline();
+
+// Content of each one wallets
 var wallets = [...Array(oods)].map(_ => +readline()).sort((a, b) => a - b);
 
-if (amount > wallets.reduce((a, b) => a + b, 0)) {
+// Polyfill
+Array.prototype.sum = function () { return this.reduce((a, b) => a + b, 0); }
+
+var getContribs = (wallet) => {
+    /**
+     * part: what the Ood should pay if he has enough money
+     */
+    let part = Math.floor(amount / oods);
 
     /**
-     * Oods don't have enough money to pay for the gift
+     * contrib: how much the Ood will really pay.
+     */
+    let contrib = Math.min(part, wallet);
+
+    /**
+     * Remove the Ood and his participation from the remaining
+     */
+    amount -= contrib;
+    oods   -= 1; // oods--;
+
+    return contrib;
+};
+
+if (amount > wallets.sum()) {
+    /**
+     * Oods don't have money enough to pay for the gift.
      */
     print('IMPOSSIBLE');
-
 } else {
-
-    wallets.map((wallet) => {
-
-        /**
-         * part: what the Ood should pay if he has enough money
-         */
-        let part = Math.floor(amount / oods--);
-
-        /**
-         * contrib: how much the Ood will really pay.
-         */
-        let contrib = wallet > part ? part : wallet;
-
-        /**
-         * Remove the Ood participation from the remaining amount
-         */
-        amount -= contrib;
-
-        print(contrib);
-    });
+    /**
+     * Oods have money enough, compute & print the part of each.
+     */
+    let contribs = wallets.map(getContribs);
+    contribs.forEach((contrib) => print(contrib));
 }
