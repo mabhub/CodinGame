20028122305d0fb4635572e1713736f5b530bcf4
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-04-13 00:45:13 +0200

    Improvments for "Le pouvoir de Thor" game (4)

--- puzzles/4.js
+++ puzzles/4.js
@@ -2,64 +2,46 @@
  * Le pouvoir de Thor
  */
 
-// var log = (data) => printErr(require('util').inspect(data, { depth:3 }));
+var log = (data) => printErr(require('util').inspect(data, { depth:3 }));
 
-var App = function () {
+const getDir = (x, y) => (y > 0 ? 'S' : y < 0 ? 'N' : '') + (x > 0 ? 'E' : x < 0 ? 'W' : '');
 
-    const dirs   = {
-        '-1': { '1':'SW', '0':'W', '-1':'NW' },
-        '0':  { '1': 'S', '0': '', '-1': 'N' },
-        '1':  { '1':'SE', '0':'E', '-1':'NE' }
-    };
-    const getDir = (x, y) => dirs[x][y];
+/**
+ * Fetch initial values
+ */
+var inputs = readline().split(' ').map(n => parseInt(n));
 
-    var thor, light;
-
-    function _main () {
-        /**
-         * Fetch initial values
-         */
-        var inputs = readline().split(' ').map(n => parseInt(n));
-
-        /**
-         * Position of target (Light)
-         */
-        light = {
-            x: inputs[0],
-            y: inputs[1]
-        };
-
-        /**
-         * Initial position of Thor
-         */
-        thor = {
-            x: inputs[2],
-            y: inputs[3]
-        };
-
-        /**
-         * Game loop
-         */
-        while (true) {
-            let remainingTurns = +readline();
-
-            let m = {
-                x: Math.sign(light.x - thor.x),
-                y: Math.sign(light.y - thor.y)
-            };
-
-            let dir = getDir(m.x, m.y)
-
-            thor.x += m.x;
-            thor.y += m.y;
-
-            print(dir);
-        };
-    }
-
-    this.start = _main;
+/**
+ * Position of target (Light)
+ */
+let light = {
+    x: inputs[0],
+    y: inputs[1]
 };
 
-var app = new App;
+/**
+ * Initial position of Thor
+ */
+let thor = {
+    x: inputs[2],
+    y: inputs[3]
+};
 
-app.start();
+/**
+ * Game loop
+ */
+while (true) {
+    let remainingTurns = +readline();
+
+    let m = {
+        x: Math.sign(light.x - thor.x),
+        y: Math.sign(light.y - thor.y)
+    };
+
+    let dir = getDir(m.x, m.y)
+
+    thor.x += m.x;
+    thor.y += m.y;
+
+    print(dir);
+}
