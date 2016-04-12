af5c993b065adbe116cc1d6b2b4c40c28d9d85a8
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-04-12 23:15:12 +0200

    Improvments for "Les Clones" game (47)

--- puzzles/47.js
+++ puzzles/47.js
@@ -1,49 +1,44 @@
-(function () {
+/**
+ * Les Clones
+ */
 
-    var board = {};
-    var elevators = [];
+const inspect  = require('util').inspect;
+const log      = (data) => printErr(inspect(data, { depth:3 }));
 
-    function readInit () {
-        var inputs                = readline().split(' ');
-        board.nbFloors              = parseInt(inputs[0]); // number of floors
-        board.width                 = parseInt(inputs[1]); // width of the area
-        board.nbRounds              = parseInt(inputs[2]); // maximum number of rounds
-        board.exitFloor             = parseInt(inputs[3]); // floor on which the exit is found
-        board.exitPos               = parseInt(inputs[4]); // position of the exit on its floor
-        board.nbTotalClones         = parseInt(inputs[5]); // number of generated clones
-        board.nbAdditionalElevators = parseInt(inputs[6]); // ignore (always zero)
-        board.nbElevators           = parseInt(inputs[7]); // number of elevators
+const [
+    nbFloors,               // number of floors
+    width,                  // width of the area
+    nbRounds,               // maximum number of rounds
+    exitF,                  // floor on which the exit is found
+    exitX,                  // position of the exit on its floor
+    nbTotalClones,          // number of generated clones
+    nbAdditionalElevators,  // ignore (always zero)
+    nbElevators             // number of elevators
+] = readline().split(' ').map(n => +n);
 
-        for (var i = 0; i < board.nbElevators; i++) {
-            inputs = readline().split(' ');
-            elevators[parseInt(inputs[0])] = parseInt(inputs[1]);
-        }
-        elevators[board.exitFloor] = board.exitPos;
-    }
+/**
+ * Init elevators
+ */
+var elevators = [...Array(nbElevators)]
+    .map(_ => readline().split(' '))
+    .reduce((arr, curr) => { arr[+curr[0]] = +curr[1]; return arr; }, []);
 
-    function read () {
-        var inputs = readline().split(' ');
-        return {
-            y:   inputs[0],
-            x:   inputs[1],
-            dir: inputs[2] == 'RIGHT' ? 1 : -1
-        };
-    }
+/**
+ * Exit can be processed as elevator
+ */
+elevators[exitF] = exitX;
 
-    function readLoop () {
-        var first;
+const readClone = _ => {
+    const [y, x, d] = readline().split(' ');
+    return {
+        x:     +x,
+        floor: +y,
+        way:    d === 'RIGHT' ? 1 : -1 // 1 for right, -1 for left
+    };
+};
 
-        while (first = read()) {
-            if ((first.dir == 1 && first.x > elevators[first.y]) || (first.dir == -1 && first.x < elevators[first.y])) {
-                print('BLOCK');
-            } else if ((first.x == board.width - 1) || first.x == 0) {
-                print('BLOCK');
-            } else {
-                print('WAIT');
-            }
-        }
-    }
+const shouldBlock = (clone) => (clone.way === 1 && clone.x > elevators[clone.floor]) || (clone.way === -1 && clone.x < elevators[clone.floor]) || ((clone.x === width - 1) || clone.x === 0);
 
-    readInit();
-    readLoop();
-}());
+while (true) {
+    print(shouldBlock(readClone()) ? 'BLOCK' : 'WAIT');
+}
