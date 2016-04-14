7b5752d5d2c73dbada837f7f296c046d8f6ee514
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-04-14 20:43:48 +0200

    Improvments for "Skynet : le virus" game (1)

--- puzzles/1.js
+++ puzzles/1.js
@@ -2,100 +2,93 @@
  * Skynet : le virus
  */
 
-var App = function () {
-
-    var links = [];
-    var exits = [];
-
-    /**
-     * Return true if "node" is directly connected to node
-     * Otherwise return false.
-     */
-    var _inLink = (node, link) => (node === link[0] || node === link[1]);
-
-    /**
-     * Return true if "link" is directly connected to a gateway
-     * Otherwise return false.
-     */
-    function _isExitLink (link) {
-        let exits_num = exits.length;
-        for (let i = 0 ; i < exits_num ; i++) {
-            let exit = exits[i];
-            if (_inLink(exit, link)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Return true if "link" is directly connected to Skynet Agent
-     * Otherwise return false.
-     */
-    var _isAgentLink = (agent_node, link) => (agent_node === link[0] || agent_node === link[1]);
-
-    /**
-     * Remove link_index element from links array
-     */
-    function _deleteLink (link_index) {
-        print(links[link_index].join(' '));
-        links.splice(link_index, 1);
-    }
-
-    function _init () {
-        let inputs    = readline().split(' ');
-        let nodes_num = +inputs[0];
-        let links_num = +inputs[1];
-        let exits_num = +inputs[2];
-
-        for (let i = 0; i < links_num; i++) {
-            let inputs = readline().split(' ');
-            links.push([+inputs[0], +inputs[1]]);
-        }
-
-        for (let i = 0; i < exits_num; i++) {
-            exits.push(+readline());
-        }
-    }
-
-    function _doLoop () {
-        while (true) {
-            let agent_node   = +readline();
-            let links_num    = links.length;
-            let delete_index = false;
-
-            /**
-             * Check if agent is immediatly near exit
-             */
-            for (let i = 0 ; i < links_num ; i++) {
-                if (_isExitLink(links[i]) && _isAgentLink(agent_node, links[i])) {
-                    delete_index = i;
-                }
-            }
-
-            /**
-             * If agent is not immediatly near exit,
-             * delete any exit link
-             *
-             * TODO: Find solution to do a better choice
-             */
-            if (delete_index === false) for (let i = 0 ; i < links_num ; i++) {
-                if (_isExitLink(links[i])) {
-                    delete_index = i;
-                }
-            }
-
-            _deleteLink(delete_index);
-        }
-    }
-
-    function _main () {
-        _init();
-        _doLoop();
-    }
-
-    this.start = _main;
+let nodes = [];
+const Node = function (id) {
+    this.id       = id;
+    this.isExit   = false;
+    this.cnxCount = 1;
 };
 
-var app = new App;
-app.start();
+Node.prototype.toString = function () {
+    return this.id;
+};
+
+/**
+ * Return true if "node" is directly connected to node
+ * Otherwise return false.
+ */
+const _inLink = (node, link) => (node === link[0] || node === link[1]);
+
+/**
+ * Return true if "link" is directly connected to a gateway
+ * Otherwise return false.
+ */
+const _isExitLink = (link) => nodes[link[0]].isExit || nodes[link[1]].isExit;
+
+/**
+ * Return true if "link" is directly connected to Skynet Agent
+ * Otherwise return false.
+ */
+const _isAgentLink = (agent_node, link) => (agent_node === link[0] || agent_node === link[1]);
+
+
+// const _isLink
+/**
+ * Remove link_index element from links array
+ */
+const _deleteLink = (link_index) => {
+    links[link_index].forEach((nid) => nodes[nid].cnxCount--);
+    print(links[link_index].join(' '));
+    links.splice(link_index, 1);
+};
+
+let [nodes_num, links_num, exits_num] = readline().split(' ').map(n =>+n);
+let links = [...Array(links_num)].map(_ => readline().split(' ').map(n=>+n));
+let exits = [...Array(exits_num)].map(_ => +readline());
+
+links.forEach((link) => {
+    link.forEach((nid) => {
+        if (nodes[nid]) nodes[nid].cnxCount++;
+        else nodes[nid] = new Node(nid);
+    })
+});
+
+exits.forEach((nid) => nodes[nid].isExit = true);
+
+while (true) {
+    let agent_node   = +readline();
+    let links_num    = links.length;
+    let delete_index = false;
+
+    /**
+     * Check if agent is immediatly near exit
+     */
+    for (let i = 0 ; i < links_num ; i++) {
+        if (_isExitLink(links[i]) && _isAgentLink(agent_node, links[i])) {
+            delete_index = i;
+        }
+    }
+
+    /**
+     * If agent is not immediatly near exit,
+     * delete link to the most connected node
+     *
+     * TODO: Find solution to do a better choice
+     */
+    let mostCnx = 0;
+    if (delete_index === false) for (let i = 0 ; i < links_num ; i++) {
+        let link = links[i];
+        if (_isAgentLink(agent_node, link)) {
+            let otherNode = nodes[link[!link.indexOf(agent_node) ? 1 : 0]];
+            /**
+             * Choose the link connected to the node with the most connections
+             */
+            if (otherNode.cnxCount >= mostCnx) {
+                mostCnx = otherNode.cnxCount;
+                delete_index = i;
+            }
+        }
+    }
+
+    _deleteLink(delete_index);
+}
