fb5cb4555a2f2a7e013a1c900ee1f5ebe86f8c59
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2016-04-15 19:27:01 +0200

    Improvments for "Skynet : le virus" game (1)

--- puzzles/1.js
+++ puzzles/1.js
@@ -4,14 +4,55 @@
 
 let nodes = [];
 const Node = function (id) {
+
     this.id       = id;
     this.isExit   = false;
-    this.cnxCount = 1;
+    this.siblings = {};
+
+    nodes[this.id] = this;
+
+    Object.defineProperties(this, {
+        cnxCount: { get: _ => Object.getOwnPropertyNames(this.siblings).length }
+    });
+
+    Node.prototype.toString = function () { return this.id; };
+
+    Node.prototype.addSibling = function (node) {
+        if (!this.siblings[node.id]) this.siblings[node.id] = node;
+        if (!node.siblings[this.id]) node.addSibling(this);
+    };
+
+    Node.prototype.remSibling = function (nid) {
+        if (this.siblings[nid]) delete this.siblings[nid];
+        if (nodes[nid].siblings[this.id]) delete nodes[nid].siblings[this.id];
+    };
 };
 
-Node.prototype.toString = function () {
-    return this.id;
+const log = n => printErr(require('util').inspect(n));
+
+const BFS = function (startNode) {
+    let nodeQueue = [];
+    let distance = 0;
+    nodeQueue.push(startNode);
+    startNode.mark = true;
+
+    while (nodeQueue.length) {
+        let currentNode = nodeQueue.shift();
+        log(currentNode.id + ':' + currentNode.isExit + ' ' + distance);
+
+        for (let nid in currentNode.siblings) {
+            let currentSibling = currentNode.siblings[nid];
+            if (!currentSibling.mark) {
+                nodeQueue.push(currentSibling);
+                currentSibling.mark = true;
+            }
+        }
+
+    }
 };
+
+
+
 
 /**
  * Return true if "node" is directly connected to node
@@ -36,24 +77,33 @@ const _isAgentLink = (agent_node, link) => (agent_node === link[0] || agent_nod
 /**
  * Remove link_index element from links array
  */
-const _deleteLink = (link_index) => {
-    links[link_index].forEach((nid) => nodes[nid].cnxCount--);
-    print(links[link_index].join(' '));
+const _cutLink = (link_index) => {
+    let link = links[link_index];
+    let n1   = nodes[link[0]];
+    let n2   = nodes[link[1]];
+
+    n1.remSibling(n2);
+    print(link.join(' '));
     links.splice(link_index, 1);
 };
 
 let [nodes_num, links_num, exits_num] = readline().split(' ').map(n =>+n);
-let links = [...Array(links_num)].map(_ => readline().split(' ').map(n=>+n));
-let exits = [...Array(exits_num)].map(_ => +readline());
 
+let links = [...Array(links_num)].map(_ => readline().split(' ').map(n=>+n));
 links.forEach((link) => {
-    link.forEach((nid) => {
-        if (nodes[nid]) nodes[nid].cnxCount++;
-        else nodes[nid] = new Node(nid);
-    })
+    let n1id = link[0];
+    let n2id = link[1];
+    let n1 = nodes[n1id] || new Node(n1id);
+    let n2 = nodes[n2id] || new Node(n2id);
+
+    n1.addSibling(n2);
 });
 
+let exits = [...Array(exits_num)].map(_ => +readline());
 exits.forEach((nid) => nodes[nid].isExit = true);
+
+
+BFS(nodes[11]);
 
 while (true) {
     let agent_node   = +readline();
@@ -90,5 +140,5 @@ while (true) {
         }
     }
 
-    _deleteLink(delete_index);
+    _cutLink(delete_index);
 }
